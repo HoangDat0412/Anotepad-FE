04a7d1b7770fc3f7d558572600503368587224a6
HoangDat0412 <hoang2811dat@gmail.com>
2024-05-27 15:52:51 +0700

    add apikey timy

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,5 +4,5 @@ export const DOMAIN = "https://htad.id.vn/api/anotepad";
 export const DOMAIN_FE = 'https://smartnote-beta.vercel.app'
 export const TOKEN = "accessToken";
 export const USER_LOGIN = "USER_LOGIN";
-export const APIKEY_TIMYCLOUND = 'lkc2uj0d0u0122kop01hqjs0jrkswomhbms5vk6q3jx3so35'
+export const APIKEY_TIMYCLOUND = 'o082ppw8qmld9av1td25p5lrvx5h9boj6366do437zjg5v8r'
   
